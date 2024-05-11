29190923812f291596ede581e4597a8633a60c23
nikolanekudova <nikolka.nekudova@seznam.cz>
2024-05-11 15:49:27 +0200

    feat(): autocorellation fixed, sound effect added and some small other features added

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,22 +1,8 @@
 import React from "react";
 
 export function StartPage(appStart, setAppStart) {
-    function getLocalStream() {
-        navigator.mediaDevices
-            .getUserMedia({ video: false, audio: true })
-            .then((stream) => {
-                window.localStream = stream;
-                window.localAudio.srcObject = stream;
-                window.localAudio.autoplay = true;
-            })
-            .catch((err) => {
-                console.error(`you got an error: ${err}`);
-            });
-    }
-
     function startApp() {
         appStart.setAppStart(true);
-        getLocalStream();
 
         console.log("app started");
     }
--- src/components/TrainerPage.jsx
+++ src/components/TrainerPage.jsx
@@ -1,14 +1,118 @@
 import React, { useState, useEffect } from "react";
-import createTuner from "@pedroloch/tuner";
 import { data } from "../data";
+import soundEffect from "../assets/correct.mp3"
+import { CorrectPage } from "./CorrectPage";
 
 export function TrainerPage(appStart, setAppStart) {
-    const tuner = createTuner();
+    const [frequency, setFrequency] = useState(0);
     const [noteStringFrequency, setNoteStringFrequency] = useState({
         note: "",
         string: 0,
-        frequency: 0,
+        frequency: -100,
     });
+    const [showCorrect, setShowCorrect] = useState(false);
+
+    function playSound() {
+        var audio = new Audio(soundEffect);
+
+        audio.play();
+    }
+
+    useEffect(() => {
+        if (Math.abs(noteStringFrequency.frequency - frequency) <= 10) {
+            playSound();
+            setShowCorrect(true);
+
+            setTimeout(() => {
+                setShowCorrect(false);
+            }, 2000);
+
+            setNoteStringFrequency(generateRandomNoteString(data));
+        }
+    }, [frequency]);
+
+    useEffect(() => {
+        let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        let microphoneStream = null;
+        let analyserNode = audioCtx.createAnalyser();
+        let audioData = new Float32Array(analyserNode.fftSize);
+
+        setNoteStringFrequency(generateRandomNoteString(data));
+        startPitchDetection();
+
+        function startPitchDetection() {
+            navigator.mediaDevices
+                .getUserMedia({ audio: true })
+                .then((stream) => {
+                    microphoneStream = audioCtx.createMediaStreamSource(stream);
+                    microphoneStream.connect(analyserNode);
+
+                    audioData = new Float32Array(analyserNode.fftSize);
+
+                    setInterval(() => {
+                        analyserNode.getFloatTimeDomainData(audioData);
+
+                        let pitch = autoCorrelate(audioData, audioCtx.sampleRate);
+
+                        if (pitch < 1000) {
+                            setFrequency(pitch);
+                        }
+                    }, 500);
+                })
+                .catch((err) => {
+                    console.log(err);
+
+                    alert("You have to enable the microphone. 🎤");
+                    stopTuner();
+                });
+        }
+
+        function autoCorrelate(buf, sampleRate) {
+            // ACF2+ algorithm
+            let SIZE = buf.length;
+            let rms = 0;
+        
+            for (let i = 0; i < SIZE; i++) {
+                let val = buf[i];
+                rms += val*val;
+            }
+
+            rms = Math.sqrt(rms/SIZE);
+            if (rms < 0.00015) // not enough signal
+                return;
+        
+            let r1 = 0, r2 = SIZE-1, thres = 0.2;
+            for (let i = 0; i < SIZE/2; i++)
+                if (Math.abs(buf[i]) < thres) { r1 = i; break; }
+            for (let i = 1; i < SIZE/2; i++)
+                if (Math.abs(buf[SIZE-i])<thres) { r2=SIZE-i; break; }
+        
+            buf = buf.slice(r1,r2);
+            SIZE = buf.length;
+        
+            let c = new Array(SIZE).fill(0);
+            for (let i = 0; i < SIZE; i++)
+                for (let j = 0; j < SIZE-i; j++)
+                    c[i] = c[i] + buf[j] * buf[j + i];
+        
+            let d = 0; while (c[d] > c[d + 1]) d++;
+            let maxval = -1, maxpos = -1;
+            for (let i = d; i < SIZE; i++) {
+                if (c[i] > maxval) {
+                    maxval = c[i];
+                    maxpos = i;
+                }
+            }
+            let T0 = maxpos;
+            let x1 = c[T0 - 1], x2 = c[T0], x3 = c[T0 + 1];
+            let a = (x1 + x3 - 2*x2) / 2;
+            let b = (x3 - x1) / 2;
+
+            if (a) T0 = T0 - b / (2 * a);
+        
+            return Math.round(sampleRate/T0);
+        }
+    }, []);
 
     function generateRandomNoteString(data) {
         function getRandomItem(array) {
@@ -28,48 +132,31 @@ export function TrainerPage(appStart, setAppStart) {
     }
 
     function stopTuner() {
-        tuner.stop();
         appStart.setAppStart(false);
-
-        console.log(tuner.isOn);
-
-        // tuner does not stop!!!
         location.reload();
     }
 
-    function getDataFromTuner() {
-        tuner.getData((tunerData) => {
-            setNoteStringFrequency(prevState => {
-                const freqencyDiff = Math.abs(
-                    tunerData.frequency - prevState.frequency
-                );
-    
-                if (freqencyDiff <= 5) {
-                    console.log("Note match!");
-
-                    return generateRandomNoteString(data);
-                } else {
-                    return prevState; // Keep the current state
-                }
-            });
-        });
-    }
-
-    tuner.start();
-
-    useEffect(() => {
-        const newNoteString = generateRandomNoteString(data);
-        setNoteStringFrequency(newNoteString);
-    
-        getDataFromTuner();
-    }, []);
-
     return (
-        <div className="trainer-page-wrapper">
-            <button onClick={stopTuner}>Stop Trainer 🛑</button>
-            <div className="notes-strings-text">
-                {noteStringFrequency.note} note on {noteStringFrequency.string}{" "}
-                string
+        <div>
+            {showCorrect && (
+                <CorrectPage />
+            )}
+            <div className="trainer-page-wrapper">
+                <button onClick={stopTuner}>Stop Trainer 🛑</button>
+                <div className="notes-strings-text">
+                    {noteStringFrequency.note} note on {noteStringFrequency.string}{" "}
+                    string
+                </div>
+                <div className="frequency">
+                    <div className="frequency-wrapper">
+                        <div className="frequency-text">Your Frequency:</div>
+                        <div>{frequency}</div>
+                    </div>
+                    <div className="frequency-wrapper">
+                        <div className="frequency-text">Should be:</div>
+                        <div>{noteStringFrequency.frequency}</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
