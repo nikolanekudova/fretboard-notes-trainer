21bdfb20aceebf61dadd4cf2ca230ef132eb059f
nikolanekudova <nikolka.nekudova@seznam.cz>
2024-06-09 16:26:52 +0200

    pwa src icons fixed

--- manifest.js
+++ manifest.js
@@ -8,25 +8,25 @@ export const manifestForPlugIn = {
         description: "Fretboard Notes Trainer",
         icons: [
             {
-                src: "/android-chrome-192x192.png",
+                src: "/fretboard-notes-trainer/android-chrome-192x192.png",
                 sizes: "192x192",
                 type: "image/png",
                 purpose: "favicon",
             },
             {
-                src: "/android-chrome-512x512.png",
+                src: "/fretboard-notes-trainer/android-chrome-512x512.png",
                 sizes: "512x512",
                 type: "image/png",
                 purpose: "favicon",
             },
             {
-                src: "/apple-touch-icon.png",
+                src: "/fretboard-notes-trainer/apple-touch-icon.png",
                 sizes: "180x180",
                 type: "image/png",
                 purpose: "apple touch icon",
             },
             {
-                src: "/maskable_icon.png",
+                src: "/fretboard-notes-trainer/maskable_icon.png",
                 sizes: "512x512",
                 type: "image/png",
                 purpose: "any maskable",
