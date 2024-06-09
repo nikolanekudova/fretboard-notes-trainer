cf867c822ce6c6b572f729b73824bc09b9ff6255
nikolanekudova <nikolka.nekudova@seznam.cz>
2024-06-09 16:47:42 +0200

    pwa changed

--- manifest.js
+++ manifest.js
@@ -1,41 +1,39 @@
 // PWA
 export const manifestForPlugIn = {
-    registerType: "prompt",
     includeAssests: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
     manifest: {
-        name: "Fretboard Notes Trainer",
-        short_name: "fretboard",
-        description: "Fretboard Notes Trainer",
-        icons: [
-            {
-                src: "/fretboard-notes-trainer/android-chrome-192x192.png",
-                sizes: "192x192",
-                type: "image/png",
-                purpose: "favicon",
-            },
-            {
-                src: "/fretboard-notes-trainer/android-chrome-512x512.png",
-                sizes: "512x512",
-                type: "image/png",
-                purpose: "favicon",
-            },
-            {
-                src: "/fretboard-notes-trainer/apple-touch-icon.png",
-                sizes: "180x180",
-                type: "image/png",
-                purpose: "apple touch icon",
-            },
-            {
-                src: "/fretboard-notes-trainer/maskable-icon.png",
-                sizes: "512x512",
-                type: "image/png",
-                purpose: "any maskable",
-            },
+        "name": "Fretboard Notes Trainer",
+        "short_name": "Fretboard Notes Trainer",
+        "icons": [
+          {
+            "src": "/fretboard-notes-trainer/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/fretboard-notes-trainer/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/fretboard-notes-trainer/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/fretboard-notes-trainer/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
         ],
-        theme_color: "#ffffff",
-        display: "standalone",
-        scope: "/fretboard-notes-trainer/",
-        start_url: "/fretboard-notes-trainer/",
-        orientation: "portrait",
-    },
+        "start_url": "/fretboard-notes-trainer/",
+        "display": "standalone",
+        "background_color": "#FFFFFF",
+        "theme_color": "#FFFFFF",
+        "description": "The application is used to practice finding notes on the neck of the guitar."
+      }
 };
