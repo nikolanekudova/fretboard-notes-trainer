b45ae8682b9f1b99a26ec6ef8ff7efa8514bd0e6
nikolanekudova <nikolka.nekudova@seznam.cz>
2024-06-15 19:28:08 +0200

    added background images and new font, css fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,28 @@ function App() {
     const [appStart, setAppStart] = useState(false);
 
     return (
-        <div className="page-wrapper">
-            <h1>Fretboard Notes Trainer 🎸</h1>
-            {appStart === false && (
-                <StartPage appStart={appStart} setAppStart={setAppStart} />
-            )}
-            {appStart === true && (
-                <TrainerPage appStart={appStart} setAppStart={setAppStart} />
-            )}
+        <div className={`${appStart === false ? "background-wrapper" : "background-wrapper-light"}`}>
+            <div className="page-wrapper">
+                <div>
+                    <h1>Fretboard Notes Trainer 🎸</h1>
+                    {appStart === false && (
+                        <p className="about-trainer">
+                            Practice makes perfect. Practice your guitar
+                            fretboard notes or, if you're confident, play a
+                            points-based game (sorry, gameplay is in progress 👀).
+                        </p>
+                    )}
+                </div>
+                {appStart === false && (
+                    <StartPage appStart={appStart} setAppStart={setAppStart} />
+                )}
+                {appStart === true && (
+                    <TrainerPage
+                        appStart={appStart}
+                        setAppStart={setAppStart}
+                    />
+                )}
+            </div>
         </div>
     );
 }
